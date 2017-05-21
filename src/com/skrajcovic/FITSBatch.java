e87ce5774f1178c247ca32607b5fbb186cb31446
sstano <s.krajcovic93@gmail.com>
2017-05-21 14:55:26 +0200

    add poznamky + regression progression

--- src/com/skrajcovic/FITSBatch.java
+++ src/com/skrajcovic/FITSBatch.java
@@ -1,6 +1,8 @@
 package com.skrajcovic;
 
+import com.sun.corba.se.impl.encoding.OSFCodeSetRegistry;
 import org.apache.commons.math3.stat.regression.SimpleRegression;
+import sun.java2d.pipe.SpanShapeRenderer;
 
 import java.util.*;
 
@@ -8,10 +10,10 @@ public class FITSBatch {
     private HashMap<double[], FITSObject> data;
     private Set<FITSObject> fSet;
     private Set<FITSObject> sSet;
-    private Set<SimpleRegression> regressions;
+    private Map<FITSObject[], SimpleRegression> regressions;
 
     public FITSBatch() {
-        regressions = new HashSet<>();
+        regressions = new HashMap<>();
         data = new HashMap<>();
         fSet = new HashSet<>();
         sSet = new HashSet<>();
@@ -21,32 +23,51 @@ public class FITSBatch {
         this.data = data;
     }
 
-    public void getPoints() {
+    public void doTheThing() {
 
-        for (Map.Entry<double[], FITSObject> e : data.entrySet()) {
-            //pass for now
-//            if (e.getKey()[1] == m*e.getKey()[0] + c) {
-                //regression check
-//            }
-        }
+        findRegressions();
 
-        System.out.println(fSet);
-
-        System.out.println(sSet);
-
-        findreal();
+        testRegressions();
     }
 
-    void findreal() {
+    void findRegressions() {
         for (FITSObject obj1 : fSet) {
             for (FITSObject obj2 : sSet) {
                 SimpleRegression sr = new SimpleRegression();
                 sr.addData(obj1.getX(), obj1.getY());
                 sr.addData(obj2.getX(), obj2.getY());
-                regressions.add(sr);
+                regressions.put(new FITSObject[]{obj1, obj2}, sr);
             }
         }
-        System.out.println(regressions.size());
+    }
+
+    void testRegressions() {
+        double threshold = 15;
+        for (Map.Entry<FITSObject[], SimpleRegression> regression : regressions.entrySet()) {
+            Set<FITSObject> result = new HashSet<>();
+            int real = 0;
+            for (FITSObject fitsObject : data.values()) {
+//                if (fitsObject != regression.getKey()[0] && fitsObject != regression.getKey()[1]) {
+                    double x = fitsObject.getX();
+                    double y = fitsObject.getY();
+                    double m = regression.getValue().getSlope();
+                    double c = regression.getValue().getIntercept();
+
+                    if (y <= (m * x + c) + threshold && y >= (m * x + c) - threshold) {
+                        if (x == 599.61 || x == 598.63 || x == 599.79 || x == 600.07 || x == 599.98 || x == 600.62) {
+                            real++;
+                        }
+                        result.add(fitsObject);
+                    }
+                }
+                if (result.size() > 0) {
+                    System.out.println(result);
+                    System.out.println(result.size());
+                    System.out.println(real);
+                    System.out.println("-------------");
+                }
+            }
+//        }
     }
 
     public void mainDataInsert(double[] id, FITSObject object) {
@@ -66,10 +87,10 @@ public class FITSBatch {
     }
 
     public String toString() {
-        for (double[] d : data.keySet()) {
-            System.out.println(String.valueOf(d[0]).replace(".", ",") + " " + String.valueOf(d[1]).replace(".", ","));
-        }
-        System.out.println();
+//        for (double[] d : data.keySet()) {
+//            System.out.println(String.valueOf(d[0]).replace(".", ",") + " " + String.valueOf(d[1]).replace(".", ","));
+//        }
+//        System.out.println();
         return this.data.toString();
     }
 }
--- src/com/skrajcovic/Main.java
+++ src/com/skrajcovic/Main.java
@@ -5,8 +5,7 @@ public class Main {
 
     public static void main(String[] args) {
     	FITSBatch batch = new FITSBatch();
-	    FileReader.processFile(batch, "Data_20120318_2.txt");
-	    System.out.println(batch);
-	    batch.getPoints();
+	    FileReader.processFile(batch, "Data_20120305_2.txt");
+	    batch.doTheThing();
     }
 }
