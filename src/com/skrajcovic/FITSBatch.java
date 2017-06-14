577d7f93c8d90b7db7ee6102681d9d7785c33e6d
krajcovic46 <s.krajcovic93@gmail.com>
2017-06-14 21:35:25 +0200

    slight refactor

--- src/com/skrajcovic/FITSBatch.java
+++ src/com/skrajcovic/FITSBatch.java
@@ -50,15 +50,8 @@ public class FITSBatch {
             Set<FITSObject> result = new HashSet<>();
             int real = 0;
             for (FITSObject fitsObject : data) {
-                double x = fitsObject.getX();
-                double y = fitsObject.getY();
-                double m = regression.getValue().getSlope();
-                double c = regression.getValue().getIntercept();
-                double b = (y > 0) ? 1 : -1;
 
-                double distance = (-m * x + b * y - c) / Math.sqrt(Math.pow(m, 2) + Math.pow(b, 2));
-
-                if (Math.abs(distance) <= threshold) {
+                if (isObjectUnderThreshold(fitsObject, regression.getValue(), threshold)) {
                     if (fitsObject.isReal()) {
                         real++;
                     }
@@ -67,14 +60,21 @@ public class FITSBatch {
             }
             if (regression.getKey()[0].isReal() && regression.getKey()[1].isReal()) {
                 return new double[]{result.size(), real, (real / (double) result.size())*100};
-//                    System.out.println(Arrays.toString(regression.getKey()) + " -- " + result);
-//                    System.out.println(result.size());
-//                    System.out.println(real);
-//                    System.out.println("Success rate: " + (real / (double) result.size()) * 100 + "%");
-//                    System.out.println("-------------");
             }
         }
         return null;
+    }
+
+    private boolean isObjectUnderThreshold(FITSObject fitsObject, SimpleRegression regression, double threshold) {
+        double x = fitsObject.getX();
+        double y = fitsObject.getY();
+        double m = regression.getSlope();
+        double c = regression.getIntercept();
+        double b = (y > 0) ? 1 : -1;
+
+        double distance = (-m * x + b * y - c) / Math.sqrt(Math.pow(m, 2) + Math.pow(b, 2));
+
+        return Math.abs(distance) <= threshold;
     }
 
     public void mainDataInsert(FITSObject object) {
