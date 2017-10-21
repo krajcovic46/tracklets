298d0d799c0bb5ebcb7b3a4795a40d58cb24ade4
krajcovic46 <s.krajcovic93@gmail.com>
2017-10-21 14:13:12 +0200

    add script for displaying stacked fits files

--- scripts/fitsdisplay.py
+++ scripts/fitsdisplay.py
@@ -1,13 +1,29 @@
 import numpy as np
 import matplotlib
+import os
 # matplotlib.rc_file("../../templates/matplotlibrc")
 import matplotlib.pyplot as plt
-
 from astropy.io import fits
-hdu_list = fits.open("../misc/FIELD01_R.091")
-image_data = hdu_list[0].data
-hdu_list.close()
 
-plt.imshow(image_data, cmap="gray")
+fits_list = []
+for filename in os.listdir("frames/20120305/GEO_tracking/Object_1"):
+    if filename.startswith("FIELD"):
+        fits_list.append("frames/20120305/GEO_tracking/Object_1/" + filename)
 
+image_concat = []
+for image in fits_list:
+    image_concat.append(fits.getdata(image))
+
+final_image = np.amax(image_concat, axis=0)
+
+# hdu_list = fits.open("frames.tar/frames/20120305/GEO_tracking/Object_1/FIELD01_R.091")
+# final_image = hdu_list[0].data
+# hdu_list.close()
+
+plt.imshow(final_image, cmap="gray", vmin=4200, vmax=5000)
 plt.colorbar()
+plt.show()
+#
+NBINS = 1000
+histogram = plt.hist(final_image.flatten(), NBINS)
+plt.show()
